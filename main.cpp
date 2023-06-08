e946276eb9e7c72f9fca394de0eb48ba9bec376b
A01749924 <117104148+A01749924@users.noreply.github.com>
2023-06-08 14:15:02 -0600

    Leer csv
    Leer csv

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main(){
+    //Abrir datos
+    ifstream entrada("DatosPeliculas.csv");
+    string linea,l0,l1,l2,l3,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10;
+    string cols[10] = {c1,c2,c3,c4,c5,c6,c7,c8,c9,c10};
+    getline(entrada,linea);        //Obtener la línea del título para descartar
+
+    while (getline(entrada,linea))
+    {
+        stringstream ss(linea);
+        
+        for (int i=0;i<10;i++){
+            getline(ss,cols[i],',');
+            cout << cols[i] << " " ;     
+        }
+        cout << endl << endl;
+    }
+    entrada.close();
+    return 0;
+}
