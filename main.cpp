9474f7cb4a23070278d92b602bc20384a1480541
A01749924 <117104148+A01749924@users.noreply.github.com>
2023-06-17 02:25:01 -0600

    Comentarios
    Comentarios en el main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+Autor: José Luis Del Alto Ordorica      A01749924       ITD
+Situación Problema
+Archivo principal*/
+
+//Incluir librerías y .h's
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -8,6 +14,7 @@
 #include "Serie.h"
 using namespace std;
 
+//Crear función para mostrar menú
 void desplegarMenu()
 {
     cout << endl
@@ -27,9 +34,11 @@ void desplegarMenu()
 
 int main()
 {
+    //Desplegar mensaje inicial
     cout << endl
          << endl
          << "Bienvenid@ a la base de datos del servicio de streaming" << endl;
+    //Inicializar vectores y variables que se usan dentro y fuera de las estructuras de control
     vector<string> nombresPelis;
     vector<string> nombresSeries;
     vector<Video *> videos;
@@ -37,15 +46,20 @@ int main()
     int opcionMenu;
     bool catalogoCargado = false;
     bool continuar = true;
+
+    //Ciclo del menú que se repetirá hasta elegir la opción 7
     while (continuar == true)
     {
+        //Imprimir menú con la función y solicitar input
         desplegarMenu();
         cout << "Ingrese la opción deseada: ";
         cin >> opcionMenu;
+        //Verificar que el input sea válido
         if (opcionMenu < 1 || opcionMenu > 7)
         {
             cout << "OPCIÓN NO EXISTENTE" << endl;
         }
+        //Terminar el ciclo en el caso de que se ingrese 7
         else if (opcionMenu == 7)
         {
             cout << "Muchas gracias por utilizar el servicio";
@@ -53,8 +67,10 @@ int main()
         }
         else
         {
+            //Ver casos en los que no se carga el catálogo
             if (catalogoCargado == false)
             {
+                //Cargar catálogo cuando el input es 1
                 if (opcionMenu == 1)
                 {
                     cout << "Cátalogo cargado exitosamente" << endl;
@@ -64,16 +80,16 @@ int main()
                     string linea, l0, l1, l2, l3;
                     string cols[10] = {};
                     getline(entrada, linea); // Obtener la línea del título para descartar
-
+                    //Leer archivo
                     while (getline(entrada, linea))
                     {
+                        //Convertir línea a stream
                         stringstream ss(linea);
-
                         for (int i = 0; i < 10; i++)
                         {
                             getline(ss, cols[i], ',');
                         }
-                        // cout << endl << endl;
+                        //Ver si es película o serie
                         if (cols[8] == "")
                         {
                             string tempID = cols[0];
@@ -83,9 +99,12 @@ int main()
                             double tempCalificacion = stod(cols[4]);
                             string tempRelease = cols[5];
 
+                            //Crear objeto Pelicula dinámicamente con los datos 
                             Pelicula *peli = new Pelicula(tempID, tempNombre, tempGenero, tempCalificacion, tempDuracion, tempRelease);
+                            //Anexarlo al vector de referencias
                             videos.push_back(peli);
                             int dentro = 0;
+                            //Crear vector de los nombres para imprimir sin formato ágilmente
                             for (int i = 0; i < size(nombresPelis); i++)
                             {
                                 if (tempNombre == nombresPelis[i])
@@ -100,6 +119,7 @@ int main()
                         }
                         else
                         {
+                            //Almacenar datos en variables
                             string tempID = cols[0];
                             string tempNombre = cols[1];
                             int tempDuracion = stoi(cols[2]);
@@ -111,10 +131,12 @@ int main()
                             int tempTemporada = stoi(cols[8]);
                             int tempEpisodio = stoi(cols[9]);
 
+                            //Crear objeto Serie dinámicamente con los datos 
                             Serie *ser = new Serie(tempID, tempNombre, tempGenero, tempCalificacion, tempDuracion, tempRelease, tempIDEpisodio,
                                                    tempNombreEpisodio, tempTemporada, tempEpisodio);
                             videos.push_back(ser);
                             int dentro = 0;
+                            //Crear vector de los nombres para imprimir sin formato ágilmente
                             for (int i = 0; i < size(nombresSeries); i++)
                             {
                                 if (tempNombre == nombresSeries[i])
@@ -128,22 +150,28 @@ int main()
                             }
                         }
                     }
+                    //Cerrar archivo
                     entrada.close();
-                    tam = size(videos);
+                    tam = size(videos);     //Guardar el tamaño final del vector como variable
                 }
                 else
+                //En el caso de no haber cargado el catálogo y seleccionar cualquier opción, mostrar
+                //este mensaje y reiniciar el ciclo del menú
                 {
                     cout << "El cátalogo no ha sido cargado" << endl;
                 }
             }
             else
             {
+                //Si ya se cargó el menú y se solicita volver a cargar, mostrar mensaje
                 if (opcionMenu == 1)
                 {
                     cout << "Cátalogo previamente cargado" << endl;
                 }
+                //Filtrar por género y calificación
                 else if (opcionMenu == 2)
                 {
+                    //Imprimir opciones de géneros
                     cout << endl
                          << endl
                          << "FILTRAR POR GÉNERO" << endl
@@ -167,8 +195,11 @@ int main()
                          << "16) Biografía\n"
                          << endl
                          << "Ingrese el número del género deseado: ";
+                         //Solicitar género
+                    //Declarar variables: string del género e int de la opción
                     string genDeseado;
                     int input;
+                    //Ciclo while para verificar que el input sea un número válido con ifs para asignar valores al string de género
                     while (true)
                     {
                         cin >> input;
@@ -257,7 +288,9 @@ int main()
                             cout << "Valor inválido, ingrese un número entero del 1-16: ";
                         }
                     }
+                    //Declarar variable de calificación
                     double califDeseada2 = 0;
+                    //Verificar con ciclo while que la calificación ingresada sea posible
                     while (califDeseada2 < 1 || califDeseada2 > 7)
                     {
                         cout << "Ingrese la calificación mínima a filtrar: ";
@@ -267,9 +300,11 @@ int main()
                             cout << "La calificación no es válida. Tiene que estar entre 1 y 7" << endl;
                         }
                     }
+                    //Crear variables temporales para comparar los inputs con los atributos de los objetos
                     string genTemp;
                     double califTemp;
                     cout << "Mostrando todos los videos del género " << genDeseado << " con una calificación mayor a " << califDeseada2 << endl;
+                    //Recorrer vector. Si se cumplen ambas condiciones, imprimir video con el método virtual
                     for (int i = 0; i < tam; i++)
                     {
                         genTemp = videos[i]->getGenero();
@@ -284,14 +319,18 @@ int main()
                         }
                     }
                 }
+                //Mostrar capítulos de una serie
                 else if (opcionMenu == 3)
                 {
+                    //Desplegar series para que el usuario pueda seleccionar
                     for (int i = 0; i < size(nombresSeries); i++)
                     {
                         cout << nombresSeries[i] << endl;
                     }
+                    //Declarar variables
                     int adentro3 = 0;
                     string serieSeleccionada3;
+                    //Verificar con ciclo que el nombre se encuentre en el vector de nombres
                     while (adentro3 == 0)
                     {
                         cout << "Ingrese el nombre de la serie deseada: ";
@@ -313,10 +352,13 @@ int main()
                     }
 
                     cout << endl;
+                    //Recorrer vector de apuntadores tipo video
                     for (int i = 0; i < tam; i++)
                     {
+                        //Si es posible, hacer upcasting de video a serie
                         if (Serie *serieVector3 = dynamic_cast<Serie *>(videos[i]))
                         {
+                            //Si coincide el nombre de la serie con la seleccionada, imprimir el episodio
                             if (serieVector3->getNombre() == serieSeleccionada3)
                             {
                                 serieVector3->verEpisodio();
@@ -324,10 +366,13 @@ int main()
                         }
                     }
                 }
+                //Mostrar películas con calificación mayor a la seleccionada
                 else if (opcionMenu == 4)
                 {
                     cout << endl;
+                    //Inicializar variable
                     double califSeleccionada5 = 0;
+                    //Verificar que la calificación sea válida con ciclo
                     while (califSeleccionada5 < 1 || califSeleccionada5 > 7)
                     {
                         cout << "Ingrese La calificación mínima para pedir película: ";
@@ -338,10 +383,13 @@ int main()
                             cout << "La calificación no es válida. Tiene que estar entre 1 y 7" << endl;
                         }
                     }
+                    //Recorrer vector de apuntadores tipo video
                     for (int i = 0; i < tam; i++)
                     {
+                        //Hacer upcasting a película si es posible
                         if (Pelicula *peliVector5 = dynamic_cast<Pelicula *>(videos[i]))
                         {
+                            //Si la calificación es mayor a la deseada, imprimir pelicula
                             if ((peliVector5->getCalificacion()) > califSeleccionada5)
                             {
                                 peliVector5->verVideo();
@@ -349,12 +397,15 @@ int main()
                         }
                     }
                 }
+                //Calificar video
                 else if (opcionMenu == 5)
                 {
+                    //Inicializar variables
                     string nombreVidCalif, nombreEpisCalif;
                     int numEpisodios = 0;
                     int contiene = 0;
                     double oldCalif, newCalif;
+                    //Verificar con un ciclo que es válido el nombre comparando con el vector
                     while (contiene == 0)
                     {
                         cout << "Ingrese el nombre de la serie o película a calificar: ";
@@ -371,6 +422,7 @@ int main()
                             cout << "El video no está en el catálogo" << endl;
                         }
                     }
+                    //Ver si es película o serie, contando cuántas veces se repite el nombre
                     for (int i = 0; i < tam; ++i)
                     {
                         if (videos[i]->getNombre() == nombreVidCalif)
@@ -378,12 +430,16 @@ int main()
                             numEpisodios++;
                         }
                     }
+                    //Si es serie
                     if (numEpisodios > 1)
                     {
+                        //Recorrer vector de apuntadores tipo video
                         for (int i = 0; i < tam; i++)
                         {
+                            //Hacer downcasting a serie
                             if (Serie *serieVector5 = dynamic_cast<Serie *>(videos[i]))
                             {
+                                //Desplegar los episodios de la serie deseada
                                 if (serieVector5->getNombre() == nombreVidCalif)
                                 {
                                     serieVector5->verVideo();
@@ -391,14 +447,18 @@ int main()
                             }
                         }
                         contiene = 0;
+                        //Verificar con un ciclo que el episodio deseado sea de la serie correspondiente
                         while (contiene == 0)
                         {
                             cout << "Ingrese el nombre del capítulo de la serie " << nombreVidCalif << " a calificar: ";
                             getline(cin, nombreEpisCalif);
+                            //Recorrer vector de apuntadores tipo video
                             for (int i = 0; i < tam; i++)
                             {
+                                //Hacer upcasting a serie si se puede
                                 if (Serie *serieVector3 = dynamic_cast<Serie *>(videos[i]))
                                 {
+                                    //Verificar que el episodio coincida
                                     if (serieVector3->getNombreEpisodio() == nombreEpisCalif)
                                     {
                                         contiene++;
@@ -410,14 +470,19 @@ int main()
                                 cout << "El episodio no está en el catálogo" << endl;
                             }
 
+                            //Recorrer vector de apuntadores tipo video
                             for (int i = 0; i < tam; i++)
                             {
+                                //Hacer upcasting a serie si se puede
                                 if (Serie *serieVector5 = dynamic_cast<Serie *>(videos[i]))
                                 {
+                                    //Ver si el nombre del episodio coincide
                                     if ((serieVector5->getNombreEpisodio() == nombreEpisCalif) && (serieVector5->getNombre() == nombreVidCalif))
                                     {
+                                        //Guardar calificación vieja en una variable
                                         oldCalif = serieVector5->getCalificacion();
                                         double newCalif = 0;
+                                        //Verificar que la calificación ingresada sea válida con ciclo while
                                         while (newCalif < 1 || newCalif > 7)
                                         {
                                             cout << "Ingrese la nueva calificación: ";
@@ -427,6 +492,7 @@ int main()
                                                 cout << "La calificación no es válida. Tiene que estar entre 1 y 7" << endl;
                                             }
                                         }
+                                        //Guardar calificación en el objeto
                                         serieVector5->setCalificacion(newCalif);
                                         cout << "La calificación de ";
                                         serieVector5->verVideo();
